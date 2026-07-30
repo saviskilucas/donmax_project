67661fd71cf1ac4cf766f8e32422fc648c9f47b6
saviski <saviskilucas@gmail.com>
2026-07-30 00:16:19 +0000

    3.2

--- app.py
+++ app.py
@@ -238,13 +238,13 @@ aba_selecionada = st.radio(
 )
 
 # Atualização silenciosa de estado sem F5
-if aba_selecionada == "🏠" and st.session_state["aba_ativa"] != "pesagem":
+if aba_selecionada == "Início" and st.session_state["aba_ativa"] != "pesagem":
     st.session_state["aba_ativa"] = "pesagem"
     st.rerun()
-elif aba_selecionada == "📋" and st.session_state["aba_ativa"] != "historico":
+elif aba_selecionada == "Planilha" and st.session_state["aba_ativa"] != "historico":
     st.session_state["aba_ativa"] = "historico"
     st.rerun()
-elif aba_selecionada == "👤" and st.session_state["aba_ativa"] != "config":
+elif aba_selecionada == "Painel" and st.session_state["aba_ativa"] != "config":
     st.session_state["aba_ativa"] = "config"
     st.rerun()
 
